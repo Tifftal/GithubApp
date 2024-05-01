d960aa89a89cad5930d2bed41cbbc3f40c9fdc19
VarTy <talankina.v@mail.ru>
2024-05-01 12:10:36 +0300

    fixed colors

--- src/pages/RepositoryPage/components/Contributors/Contributors.tsx
+++ src/pages/RepositoryPage/components/Contributors/Contributors.tsx
@@ -1,15 +1,13 @@
 import { Props } from "./types";
+import Contributor from "../Contributor/Contributor";
 
 import styles from "./styles.module.scss"
-import CircleIcon from "icons/CircleIcon";
-import Contributor from "../Contributor/Contributor";
 
 const Contributors: React.FC<Props> = ({ contributors }) => {
     return (
         <div className={styles.contributors}>
             <div className={styles.contributorTitle}>
                 Contributors
-                <CircleIcon fill="#D9D9D9" />
                 <div className={styles.contributorsNumber}>{contributors.length}</div>
             </div>
             {contributors.map((contributor, idx) => (
